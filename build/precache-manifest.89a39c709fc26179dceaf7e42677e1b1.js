9ef07b897cba410c8c0491ce467c823ee6309cfb
Nguyễn Trung Nghĩa <ntn641998@gmail.com>
2020-07-15 00:39:59 +0700

    fix test

---
+++ build/precache-manifest.89a39c709fc26179dceaf7e42677e1b1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8c8c30b70b744a4b446bdc29cfd08ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30d95e09a9af6dbd447c",
+    "url": "/static/css/2.7e7e4c02.chunk.css"
+  },
+  {
+    "revision": "65bd2993714bc19dd8b7",
+    "url": "/static/css/main.57b85398.chunk.css"
+  },
+  {
+    "revision": "30d95e09a9af6dbd447c",
+    "url": "/static/js/2.4f7ed2ea.chunk.js"
+  },
+  {
+    "revision": "65bd2993714bc19dd8b7",
+    "url": "/static/js/main.1bc4c200.chunk.js"
+  },
+  {
+    "revision": "de627c81bb6c03072aab",
+    "url": "/static/js/runtime-main.aae216e4.js"
+  },
+  {
+    "revision": "8655525e9167d04ce00d6e8792c47e86",
+    "url": "/static/media/alert-error.8655525e.svg"
+  },
+  {
+    "revision": "13b071d684a9e09e96a0b780760b07c4",
+    "url": "/static/media/alert-info.13b071d6.svg"
+  },
+  {
+    "revision": "e859e9c4dc3ee3ee9e5e621422706d38",
+    "url": "/static/media/alert-question.e859e9c4.svg"
+  },
+  {
+    "revision": "15f37392aa18337797f49cb980ac7fd9",
+    "url": "/static/media/alert-success.15f37392.svg"
+  },
+  {
+    "revision": "276241891832ecb7c968444da937a34e",
+    "url": "/static/media/alert-warning.27624189.svg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
